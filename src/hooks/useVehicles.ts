5f5449ab09b30015bf126ace1757c2adaceb9991
AceNeoTheOne <jadlcvaldivia@gmail.com>
2026-04-13 20:06:33 -0500

    Created Custom Data Fetching Hook

---
+++ src/hooks/useVehicles.ts
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import { CanceledError } from "../web-services/api-client";
+import VehiclesServices, { Vehicle } from "../web-services/vehicles";
+
+const useVehicles = () => {
+  const [vehicles, setVehicles] = useState<Vehicle[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    //GET ALL VEHICLES
+    const { request, cancel } = VehiclesServices.getSelect<Vehicle>();
+    request
+      .then((response) => {
+        setVehicles(response.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
+
+    //DELETE VEHICLE
+    //const vehicleID = "P-XX1";
+    //VehiclesServices.deleteDelete(vehicleID).catch((err) => {
+    //setError(err.message);
+    //});
+
+    //ADD VEHICLE
+    //const newVehicle = {
+    //VehicleID: "p-xx1",
+    //UA: 800,
+    //};
+    //VehiclesServices.postInsert(newVehicle)
+    //.then((response) => {
+    //setError("vehicle added");
+    //})
+    //.catch((err) => {
+    //setError(err.message);
+    //});
+
+    //UPDATE VEHICLE
+    //const conditions = "P-XX1";
+    //const vehicleData = {
+    //UA: 1000,
+    //};
+    //VehiclesServices.putUpdate(vehicleData, conditions)
+    //.then((response) => {
+    //setError("vehicle updated");
+    //})
+    //.catch((err) => {
+    //setError(err.message);
+    //});
+
+    return () => cancel();
+  }, []);
+
+  return { vehicles, error, isLoading, setVehicles, setError, setIsLoading };
+};
+export default useVehicles;
